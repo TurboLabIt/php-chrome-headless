9f44077bf8c009cc021e0ab0e690e97f63279b7d
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-04-21 00:17:50 +0200

    Update ChromeHeadless.php

--- src/ChromeHeadless.php
+++ src/ChromeHeadless.php
@@ -31,7 +31,7 @@ class ChromeHeadless
         $this->browser      = $browserFactory->createBrowser([
             'windowSize'                => [1920, 1000],
             'ignoreCertificateErrors'   => true,
-            'userAgent'                 => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.34 Safari/537.36'
+            'userAgent'                 => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36'
         ]);
         $this->logger       = $logger;
         $this->cache        = $cache;
